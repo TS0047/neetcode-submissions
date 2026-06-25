521bed280da5bd21361dae05bdf3ed9534eaa30d
Tamilselvan K Karthikeyan <tamilselvan.kkts@gmail.com>
2026-06-26 00:37:38 +0530

    Add: plus-one - submission-1

---
+++ Data Structures & Algorithms/plus-one/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        carry = 1
+        ind = len(digits) -1
+        while carry:
+            if digits[ind]<9:
+                digits[ind]+=1
+                return digits
+            if digits[ind] == 9:
+                digits[ind]=0
+                ind-=1
+                if ind == -1:
+                    digits.insert(0,1)
+                    return digits
+
+                
+
+
+
+            
+        
+        
