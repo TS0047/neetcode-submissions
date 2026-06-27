cb1c8dc76310ee847b25bff14fbe196be87df77f
Tamilselvan K Karthikeyan <tamilselvan.kkts@gmail.com>
2026-06-28 04:17:18 +0530

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.py
@@ -0,0 +1,16 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        a = sorted(intervals,key= lambda x: x.start)
+        for i in range(1,len(a)):
+            if a[i].start<a[i-1].end:
+                return False
+        return True
+
